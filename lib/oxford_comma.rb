f77d39d16be5fd675c4fd87ad64ba9552fce2a39
Devin Wilson <craigvin84@yahoo.com>
2020-10-06 22:45:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,10 @@
 def oxford_comma(array)
-  if array.length == 2
-    return "#{array[0]} and #{array[1]}"
-  elsif 2 < array.length
-    array[-1].insert(0, "and ")
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    array.push[-1] = "and #{array[-1]}"
+    array.join(", ")
   end
-  array.join(", ")
 end
